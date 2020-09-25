0d1593d5daadb71d9214833c1a4f214a5a54415f
Kinminji <minfeel0214@gmail.com>
2020-09-25 21:21:47 +0900

    galaxy
    galaxy

--- js/main.js
+++ js/main.js
@@ -77,17 +77,16 @@ function drags(dragElement, resizeElement, container) {
   });
 }
 
-
-  $(function(){
-    $('.button-bronze').click(function(){
-      $('.bronze').css("display","block");
-      $('.sliver').css("display","none");
-    });
-  });
-  $(function(){
-    $('.button-sliver').click(function(){
-      $('.bronze').css("display","none");
-      $('.sliver').css("display","block");
-    });
-  });
-
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
